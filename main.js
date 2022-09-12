e9ed6a770fdf4b069e4adf71c4dfe6dbce2a5d6e
Rustam7717 <dj_rustam85@mail.ru>
2022-09-12 18:32:54 +0600

    HomeWOrk3JS

---
+++ main.js
@@ -0,0 +1,67 @@
+
+////////////////////   First task    ////////////////
+
+
+// function compare(number1, number2){
+// if(number1>number2){
+//     alert(number1 + '  is bigger than  ' + number2)
+// }
+// else if(number1<number2){
+//     alert(number1 + '  is smaller than ' + number2)
+// }
+// else{
+//     (number1=number2)
+//         alert(number1 + ' is equal to ' + number2)
+//     }
+// }
+// const number1 = prompt('Input your first number!')
+// const number2 = prompt('Input your  second number!')
+
+// compare(number1, number2)
+
+//////////////////  End of first task /////////////
+
+
+
+////////////    Second task  //////////
+
+function twoMass (array1, array2){
+    if (array1>array2){
+        alert (array1 + ' first massive is longer than  second =' + array2)
+    } else if(array1<array2){
+        alert (array1 + ' first massive is shorter than second' + array2)
+    }else{
+        (array1=array1)
+        alert('massives are equal to each other   Aijamal poidem pit pivo')   
+    }
+}
+const array1 = prompt('Input any data')
+const array2 = prompt('Input any data to compare')
+
+twoMass(array1.length, array2.length)
+
+///////////  end of second task   ////////////
+
+
+// function countChar(argCount){
+//     let value = 0;
+//     for (let i = 1; i <= argCount; i++) {
+//         value = i;   
+//     }
+//     console.log(value);
+// }
+// const arg = prompt('Input value').replaceAll(' ', '');
+
+// countChar(arg.length)
+
+
+// function countChar(argCount){
+//     let value = 0;
+//     for (let i = 1; i <= argCount; i++) {
+//         value = i;
+//     }
+//     alert(value);
+// }
+// const arg = prompt('Input value').replaceAll(' ', '');
+
+// countChar(arg.length)
